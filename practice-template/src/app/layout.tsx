3ade29e60d8f3fc857ee885a8207a4cc8b5f2994
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-12-04 19:26:16 +0500

    updated

--- practice-template/src/app/layout.tsx
+++ practice-template/src/app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/Theme-Provider";
 import Footer from "@/components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -27,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${poppins.className} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
--- practice-template/src/components/Footer.tsx
+++ practice-template/src/components/Footer.tsx
@@ -1,10 +1,10 @@
-import SendHorizontal from "/public/SendHorizontal.svg";
+import { SendHorizontal } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
 export default function Footer() {
   return (
-    <div className="flex justify-between items-center bg-foreground px-20 py-10">
+    <div className="flex justify-between items-start bg-foreground px-20 py-10">
       <div className="grid place-items-start gap-4">
         <h3 className="text-lg font-bold text-background">Exclusive</h3>
         <div className="grid place-items-start gap-2">
@@ -12,89 +12,49 @@ export default function Footer() {
           <p className="text-sm font-light text-background">
             Get 10% off your first order
           </p>
-          <div className="flex justify-between items-center border-[1px] border-background py-1 px-[10px] mt-1 rounded-sm">
+          <div className="flex justify-between items-center border-[1px] border-background py-[6px] px-[10px] mt-1 rounded-sm">
             <input
               type="text"
               placeholder="Enter your email"
-              className="text-sm font-light bg-transparent placeholder:text-zinc-700"
+              className="text-sm font-light text-background bg-transparent placeholder:text-zinc-700"
             />
-            <Image
-              src="/public/SendHorizontal.svg"
-              alt="SendHorizontal"
-              width={20}
-              height={20}
-            />
-            <SendHorizontal className="text-background" />
+            <SendHorizontal className="text-background size-5" />
           </div>
         </div>
       </div>
-      <div className="grid place-items-start gap-4">
-        <h3 className="text-lg font-bold text-background">Exclusive</h3>
-        <div className="grid place-items-start gap-2">
-          <h4 className="font-semibold text-background">Subscribe</h4>
-          <p className="text-sm font-light text-background">
-            Get 10% off your first order
-          </p>
-          <div className="flex justify-between items-center border-[1px] border-background py-1 px-[10px] mt-1 rounded-sm">
-            <input
-              type="text"
-              placeholder="Enter your email"
-              className="text-sm font-light bg-transparent placeholder:text-zinc-700"
-            />
-            <SendHorizontal className="text-background" />
-          </div>
-        </div>
+
+      <div className="grid place-items-start gap-2">
+        <h4 className="font-semibold text-background">Support</h4>
+        <p className="text-sm font-light text-background">
+          111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.
+        </p>
+        <p className="text-sm font-light text-background">
+          exclusive@gmail.com
+        </p>
+        <p className="text-sm font-light text-background">+88015-88888-9999</p>
       </div>
-      <div className="grid place-items-start gap-4">
-        <h3 className="text-lg font-bold text-background">Exclusive</h3>
-        <div className="grid place-items-start gap-2">
-          <h4 className="font-semibold text-background">Subscribe</h4>
-          <p className="text-sm font-light text-background">
-            Get 10% off your first order
-          </p>
-          <div className="flex justify-between items-center border-[1px] border-background py-1 px-[10px] mt-1 rounded-sm">
-            <input
-              type="text"
-              placeholder="Enter your email"
-              className="text-sm font-light bg-transparent placeholder:text-zinc-700"
-            />
-            <SendHorizontal className="text-background" />
-          </div>
-        </div>
+
+      <div className="grid place-items-start gap-2">
+        <h4 className="font-semibold text-background">Account</h4>
+        <p className="text-sm font-light text-background">My Account</p>
+        <p className="text-sm font-light text-background">Login / Register</p>
+        <p className="text-sm font-light text-background">Cart</p>
+        <p className="text-sm font-light text-background">Wishlist</p>
+        <p className="text-sm font-light text-background">Shop</p>
       </div>
-      <div className="grid place-items-start gap-4">
-        <h3 className="text-lg font-bold text-background">Exclusive</h3>
-        <div className="grid place-items-start gap-2">
-          <h4 className="font-semibold text-background">Subscribe</h4>
-          <p className="text-sm font-light text-background">
-            Get 10% off your first order
-          </p>
-          <div className="flex justify-between items-center border-[1px] border-background py-1 px-[10px] mt-1 rounded-sm">
-            <input
-              type="text"
-              placeholder="Enter your email"
-              className="text-sm font-light bg-transparent placeholder:text-zinc-700"
-            />
-            <SendHorizontal className="text-background" />
-          </div>
-        </div>
+      <div className="grid place-items-start gap-2">
+        <h4 className="font-semibold text-background">Quick Link</h4>
+        <p className="text-sm font-light text-background">Privacy Policy</p>
+        <p className="text-sm font-light text-background">Terms Of Use</p>
+        <p className="text-sm font-light text-background">FAQ</p>
+        <p className="text-sm font-light text-background">Contact</p>
       </div>
-      <div className="grid place-items-start gap-4">
-        <h3 className="text-lg font-bold text-background">Exclusive</h3>
-        <div className="grid place-items-start gap-2">
-          <h4 className="font-semibold text-background">Subscribe</h4>
-          <p className="text-sm font-light text-background">
-            Get 10% off your first order
-          </p>
-          <div className="flex justify-between items-center border-[1px] border-background py-1 px-[10px] mt-1 rounded-sm">
-            <input
-              type="text"
-              placeholder="Enter your email"
-              className="text-sm font-light bg-transparent placeholder:text-zinc-700"
-            />
-            <SendHorizontal className="text-background" />
-          </div>
-        </div>
+      <div className="grid place-items-start gap-2">
+        <h4 className="font-semibold text-background">Download App</h4>
+        <p className="text-sm font-light text-background">Privacy Policy</p>
+        <p className="text-sm font-light text-background">Terms Of Use</p>
+        <p className="text-sm font-light text-background">FAQ</p>
+        <p className="text-sm font-light text-background">Contact</p>
       </div>
     </div>
   );
