174c6cfcd3aa6c720d431f4d001b611e218e2075
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-12-05 16:32:25 +0500

    updated

--- practice-template/src/components/Navbar.tsx
+++ practice-template/src/components/Navbar.tsx
@@ -5,8 +5,11 @@ import { LuSearch } from "react-icons/lu";
 import { RiHeart3Line } from "react-icons/ri";
 import { PiShoppingCart } from "react-icons/pi";
 import { RxCross2 } from "react-icons/rx";
+import { CgMenuRightAlt } from "react-icons/cg";
 import { ToggleButton } from "@/components/ToggleButton";
 import { Button } from "./ui/button";
+import { useState, useEffect } from "react";
+import { FaAngleDown } from "react-icons/fa6";
 
 const navLinks = [
   { name: "Home", path: "/" },
@@ -16,54 +19,119 @@ const navLinks = [
 ];
 
 export default function Navbar() {
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+
   const pathName = usePathname();
   const isActive = (path: string) => path == pathName;
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setIsSearchVisible(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   return (
-    <div className="relative flex justify-between items-center px-8 lg:px-20 xl:px-24 py-3 mt-5 border-b-[1px]">
-      <Link href="/" className="font-bold text-xl">
-        Exclusive
-      </Link>
-      <ul className="hidden sm:flex justify-center items-center gap-6 md:gap-8">
-        {navLinks.map((link, i) => (
-          <li
-            key={i}
-            className={`text-foreground text-sm font-medium ${
-              isActive(link.path)
-                ? "border-b-2 border-secondary"
-                : "border-b-2 border-transparent"
+    <div className="border-b-[1px]">
+      <div className="flex justify-between gap-5 bg-green-600 text-semi-text px-4 sm:px-8 lg:px-20 xl:px-24 py-3 md:items-center">
+        <div className="md:flex-1 md:text-center text-xs">
+          <p>
+            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+            <span className="ml-1 font-semibold cursor-pointer underline text-xs">
+              Shop Now
+            </span>
+          </p>
+        </div>
+        <div className="flex items-center gap-1">
+          <p className="text-xs">English</p>
+          <FaAngleDown className="text-xs" />
+        </div>
+      </div>
+
+      <div className="relative flex justify-between items-center px-4 sm:px-8 lg:px-20 xl:px-24 py-2 md:py-3 mt-3 md:mt-5">
+        {" "}
+        <Link href="/" className="font-bold text-xl">
+          Exclusive
+        </Link>
+        <ul className="hidden sm:flex justify-center items-center gap-6 md:gap-8">
+          {navLinks.map((link, i) => (
+            <li
+              key={i}
+              className={`text-foreground text-sm font-medium ${
+                isActive(link.path)
+                  ? "border-b-2 border-secondary"
+                  : "border-b-2 border-transparent"
+              }`}
+            >
+              <Link href={link.path}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+        <div className="flex justify-center items-center gap-3 md:gap-5">
+          <div className="hidden lg:flex justify-between items-center h-9 px-[10px] rounded-sm w-60 bg-muted">
+            <input
+              type="text"
+              placeholder="What are you looking for?"
+              className="placeholder:text-xs text-sm text-foreground bg-transparent placeholder:text-muted-foreground outline-none w-[80%]"
+            />
+            <LuSearch className="text-foreground text-lg" />
+          </div>
+          <LuSearch
+            className={`text-foreground text-lg sm:text-xl lg:hidden transition-all ${
+              isSearchVisible ? "text-transparent" : ""
             }`}
-          >
-            <Link href={link.path}>{link.name}</Link>
-          </li>
-        ))}
-      </ul>
-      <div className="flex justify-center items-center gap-4 md:gap-5">
-        <div className="hidden lg:flex justify-between items-center h-9 px-[10px] rounded-sm w-60 bg-muted">
+            onClick={() => {
+              setIsSearchVisible(true);
+            }}
+          />
+          <RiHeart3Line className="text-foreground text-lg sm:text-xl transition-all" />
+          <PiShoppingCart className="text-foreground text-lg sm:text-xl transition-all" />
+          <CgMenuRightAlt className="text-foreground text-lg sm:text-xl  transition-all sm:hidden" />
+
+          <ToggleButton />
+        </div>
+        <div
+          className={`absolute bottom-0 left-1/2 translate-y-10 -translate-x-1/2 justify-between items-center h-9 pl-[10px] rounded-md w-[75%] bg-muted ${
+            isSearchVisible ? "flex" : "hidden"
+          }`}
+        >
           <input
             type="text"
             placeholder="What are you looking for?"
             className="placeholder:text-xs text-sm text-foreground bg-transparent placeholder:text-muted-foreground outline-none w-[80%]"
           />
-          <LuSearch className="text-foreground text-lg" />
+          <div className="flex justify-between items-center gap-2">
+            <LuSearch className="text-foreground text-lg" />
+            <Button
+              size={"icon"}
+              onClick={() => {
+                setIsSearchVisible(false);
+              }}
+            >
+              <RxCross2 className="text-[22px]" />
+            </Button>
+          </div>
         </div>
-        <LuSearch className="text-foreground text-xl lg:hidden" />
-        <RiHeart3Line className="text-foreground text-xl" />
-        <PiShoppingCart className="text-foreground text-xl" />
-        <ToggleButton />
-      </div>
-      <div className="absolute bottom-0 left-1/2 translate-y-10 -translate-x-1/2 flex justify-between items-center h-9 pl-[10px] rounded-md w-[75%] bg-muted ">
-        <input
-          type="text"
-          placeholder="What are you looking for?"
-          className="placeholder:text-xs text-sm text-foreground bg-transparent placeholder:text-muted-foreground outline-none w-[80%]"
-        />
-        <div className="flex justify-between items-center gap-2">
-          <LuSearch className="text-foreground text-lg" />
-          <Button size={"icon"}>
-            <RxCross2 className="text-[22px]" />
-          </Button>
-        </div>
+        <ul className="absolute top-10 right-0 w-1/2 bg-green-500 hidden">
+          {navLinks.map((link, i) => (
+            <li
+              key={i}
+              className={`text-foreground text-sm font-medium ${
+                isActive(link.path)
+                  ? "border-b-2 border-secondary"
+                  : "border-b-2 border-transparent"
+              }`}
+            >
+              <Link href={link.path}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
