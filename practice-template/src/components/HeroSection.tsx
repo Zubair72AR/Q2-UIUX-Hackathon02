d85f8cdf5d104ee2be23d49ce0c5a9780226e09a
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-12-06 12:58:57 +0500

    updated

--- practice-template/src/components/HeroSection.tsx
+++ practice-template/src/components/HeroSection.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { FaAngleRight } from "react-icons/fa6";
+
+const sideMenuList = [
+  { title: "Woman’s Fashion", icon: true, subMenu: ["Dresses", "Tops"] },
+  { title: "Men’s Fashion", icon: true, subMenu: ["Shirts", "Pants"] },
+  { title: "Electronics", icon: false, subMenu: [] },
+  { title: "Home & Lifestyle", icon: false, subMenu: [] },
+  { title: "Medicine", icon: false, subMenu: [] },
+  { title: "Sports & Outdoor", icon: false, subMenu: [] },
+  { title: "Baby’s & Toys", icon: false, subMenu: [] },
+  { title: "Groceries & Pets", icon: false, subMenu: [] },
+  { title: "Health & Beauty", icon: false, subMenu: [] },
+];
 
 export default function HeroSection() {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  // Side Mene Sub Menu
+  const sideMenuCollapse = (index: number) => {
+    setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
   return (
-    <div className="h-96">
-      <h1>Hero Section</h1>
+    <div className="px-7 sm:px-8 lg:px-20 xl:px-24">
+      {/* Side Menu */}
+      <div className="w-52 space-y-3 pt-10 border-r-[1px] pr-4">
+        {sideMenuList.map((e, i) => (
+          <div key={i}>
+            <div
+              className="flex justify-between items-center cursor-pointer"
+              onClick={() => sideMenuCollapse(i)}
+            >
+              <p className="text-foreground text-sm font-medium hover:text-chart-1">
+                {e.title}
+              </p>
+
+              {/* Arrow Icon and Rotate Icon */}
+              {e.icon && (
+                <FaAngleRight
+                  className={`text-sm transition-all duration-300 ${
+                    openIndex === i ? "rotate-90" : ""
+                  }`}
+                />
+              )}
+            </div>
+
+            {/* Show Sub Menu */}
+            {openIndex === i && e.subMenu.length > 0 && (
+              <div className="space-y-1">
+                {e.subMenu.map((subItem, index) => (
+                  <p
+                    key={index}
+                    className="text-secondary-foreground bg-secondary text-xs hover:bg-muted hover:text-chart-1 px-1 py-[2px]"
+                  >
+                    {subItem}
+                  </p>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      {/* Hero Image */}
+      <div></div>
     </div>
   );
 }
--- practice-template/src/components/Navbar.tsx
+++ practice-template/src/components/Navbar.tsx
@@ -6,11 +6,14 @@ import { RiHeart3Line } from "react-icons/ri";
 import { PiShoppingCart } from "react-icons/pi";
 import { RxCross2 } from "react-icons/rx";
 import { CgMenuRightAlt } from "react-icons/cg";
+import { AiOutlineClose } from "react-icons/ai";
+
 import { ToggleButton } from "@/components/ToggleButton";
 import { Button } from "./ui/button";
 import { useState, useEffect } from "react";
 import { FaAngleDown } from "react-icons/fa6";
 
+// Nav Links Array
 const navLinks = [
   { name: "Home", path: "/" },
   { name: "Contact", path: "/contact" },
@@ -21,10 +24,31 @@ const navLinks = [
 export default function Navbar() {
   const [isSearchVisible, setIsSearchVisible] = useState(false);
   const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const [selectLang, setSelectLang] = useState("English");
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
+  // Array of Languages List
+  const languages = [
+    "English",
+    "Urdu",
+    "Arabic",
+    "German",
+    "Italian",
+    "Spanish",
+  ];
+  // Show and Hide Languages List
+  const toggleDropDownLang = () => setIsDropdownVisible((prev) => !prev);
+  // FUnction for Updating Language
+  const handleSelectLanguage = (e: string) => {
+    setSelectLang(e);
+    setIsDropdownVisible(false);
+  };
+
+  // Show Active Nav Link
   const pathName = usePathname();
   const isActive = (path: string) => path == pathName;
 
+  // Show and hide Navbar According to Media Size
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 1024) {
@@ -44,6 +68,7 @@ export default function Navbar() {
 
   return (
     <div className="border-b-[1px]">
+      {/* Top Sale Discount Offer Tab */}
       <div className="flex justify-between gap-5 bg-foreground dark:bg-chart-1 text-background dark:text-foreground px-7 sm:px-8 lg:px-20 xl:px-24 py-3 md:items-center">
         <div className="md:flex-1 md:text-center text-xs">
           <p>
@@ -53,17 +78,41 @@ export default function Navbar() {
             </span>
           </p>
         </div>
-        <div className="flex items-center gap-1">
-          <p className="text-xs">English</p>
-          <FaAngleDown className="text-xs" />
+
+        {/* Change Languages Button */}
+        <div className="relative">
+          <div
+            className="flex items-center gap-1 cursor-pointer"
+            onClick={toggleDropDownLang}
+          >
+            <p className="text-xs">{selectLang}</p>
+            <FaAngleDown className="text-xs" />
+          </div>
+          {/* list of Available Languages */}
+          {isDropdownVisible && (
+            <div className="absolute mt-1 z-30 bg-chart-5 dark:bg-foreground text-background shadow-md rounded-sm">
+              {languages.map((language, i) => (
+                <p
+                  key={i}
+                  onClick={() => handleSelectLanguage(language)}
+                  className="cursor-pointer text-xs px-3 py-1 hover:text-primary-foreground hover:bg-primary"
+                >
+                  {language}
+                </p>
+              ))}
+            </div>
+          )}
         </div>
       </div>
 
+      {/* NavBar Stars from Here */}
       <div className="relative flex justify-between items-center px-7 sm:px-8 lg:px-20 xl:px-24 py-2 md:py-3 mt-3 md:mt-4">
-        {" "}
+        {/* Nav Logo */}
         <Link href="/" className="font-bold text-xl">
           Exclusive
         </Link>
+
+        {/* Nav Links for Large Screen */}
         <ul className="hidden sm:flex justify-center items-center gap-6 md:gap-8">
           {navLinks.map((link, i) => (
             <li
@@ -78,7 +127,10 @@ export default function Navbar() {
             </li>
           ))}
         </ul>
+
+        {/* Nav Icons for Right Side */}
         <div className="flex justify-center items-center gap-3 md:gap-5">
+          {/* Search Bar and Icons for Large Screen */}
           <div className="hidden lg:flex justify-between items-center h-9 px-[10px] rounded-sm w-60 bg-muted">
             <input
               type="text"
@@ -87,6 +139,8 @@ export default function Navbar() {
             />
             <LuSearch className="text-foreground text-lg" />
           </div>
+
+          {/* Search Icons for Mobile Devices */}
           <LuSearch
             className={`text-foreground text-lg sm:text-xl lg:hidden transition-all ${
               isSearchVisible ? "text-transparent" : ""
@@ -95,18 +149,27 @@ export default function Navbar() {
               setIsSearchVisible(true);
             }}
           />
+
+          {/* Icons for Heart/Favorite, Shopping Cart HamburgerMenu */}
           <RiHeart3Line className="text-foreground text-lg sm:text-xl transition-all" />
           <PiShoppingCart className="text-foreground text-lg sm:text-xl transition-all" />
-          <CgMenuRightAlt
-            className="text-foreground text-lg sm:text-xl  transition-all sm:hidden"
+
+          {/* HamburgerMenu  */}
+          <div
+            className="text-foreground text-lg sm:text-xl transition-all sm:hidden"
             onClick={() => {
-              setIsMenuVisible(true);
+              setIsMenuVisible((prev) => !prev);
               setIsSearchVisible(false);
             }}
-          />
+          >
+            {isMenuVisible ? <AiOutlineClose /> : <CgMenuRightAlt />}
+          </div>
 
+          {/* Light and Dark Theme Toggle Button */}
           <ToggleButton />
         </div>
+
+        {/* Search Bar Appear for Mobile Devices */}
         <div
           className={`absolute bottom-0 left-1/2 translate-y-10 -translate-x-1/2 justify-between items-center h-9 pl-[10px] rounded-md w-[75%] bg-muted ${
             isSearchVisible ? "flex" : "hidden"
@@ -130,9 +193,11 @@ export default function Navbar() {
           </div>
         </div>
       </div>
+
+      {/* Nav Links for Mobile Devices */}
       <ul
-        className={`absolute top-0 right-0 h-screen w-full xs:w-1/2 bg-background	py-12 flex-col border-[1px] transition-all duration-1000 ${
-          isMenuVisible ? "flex" : "hidden"
+        className={`fixed top-0 h-screen w-full xs:w-2/3 bg-[rgba(225,225,225,0.4)] dark:bg-[rgba(0,0,0,0.4)]  backdrop-blur-sm	py-12 flex flex-col border-r-2 transition-all duration-1000 ${
+          isMenuVisible ? "left-0 shadow-lg" : "-left-full"
         }`}
       >
         <Button
@@ -140,7 +205,7 @@ export default function Navbar() {
           onClick={() => {
             setIsMenuVisible(false);
           }}
-          className="ml-8 mb-8"
+          className="ml-[75%] mb-4"
         >
           <RxCross2 />
         </Button>
