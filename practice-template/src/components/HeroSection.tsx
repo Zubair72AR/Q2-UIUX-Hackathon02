07a841ce5ceaca2f9d52360f252e57ea4713df01
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-12-06 16:44:15 +0500

    updated

--- practice-template/src/components/HeroSection.tsx
+++ practice-template/src/components/HeroSection.tsx
@@ -1,6 +1,24 @@
 "use client";
 import { useState } from "react";
 import { FaAngleRight } from "react-icons/fa6";
+import React from "react";
+import { FaApple } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+import Image01 from "../../public/phone01.png";
+import Image02 from "../../public/phone02.png";
+import Image03 from "../../public/phone03.png";
+import Image04 from "../../public/phone04.png";
+import Image05 from "../../public/phone05.png";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import Link from "next/link";
 
 const sideMenuList = [
   { title: "Woman’s Fashion", icon: true, subMenu: ["Dresses", "Tops"] },
@@ -14,8 +32,58 @@ const sideMenuList = [
   { title: "Health & Beauty", icon: false, subMenu: [] },
 ];
 
+const heroIMages = [
+  {
+    no: 1,
+    title: "iPhone 14 Series",
+    heading: "Up to 10%",
+    src: Image01,
+  },
+  {
+    no: 2,
+    title: "iPhone 13 Series",
+    heading: "Up to 15%",
+    src: Image02,
+  },
+  {
+    no: 3,
+    title: "iPhone 15 Series",
+    heading: "Up to 20%",
+    src: Image03,
+  },
+  {
+    no: 4,
+    title: "iPhone 15 Series",
+    heading: "Up to 20%",
+    src: Image04,
+  },
+  {
+    no: 5,
+    title: "iPhone 15 Series",
+    heading: "Up to 20%",
+    src: Image05,
+  },
+];
+
 export default function HeroSection() {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap());
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap());
+    });
+  }, [api]);
 
   // Side Mene Sub Menu
   const sideMenuCollapse = (index: number) => {
@@ -23,9 +91,9 @@ export default function HeroSection() {
   };
 
   return (
-    <div className="px-7 sm:px-8 lg:px-20 xl:px-24">
+    <div className="flex justify-between items-end h-full px-7 sm:px-8 lg:px-20 xl:px-24">
       {/* Side Menu */}
-      <div className="w-52 space-y-3 pt-10 border-r-[1px] pr-4">
+      <div className="w-52 space-y-3 pt-10 border-r-[1px] pr-4 relative">
         {sideMenuList.map((e, i) => (
           <div key={i}>
             <div
@@ -64,7 +132,59 @@ export default function HeroSection() {
       </div>
 
       {/* Hero Image */}
-      <div></div>
+
+      <Carousel setApi={setApi} className="w-[calc(100%-228px)]">
+        <CarouselContent>
+          {heroIMages.map((e, i) => (
+            <CarouselItem>
+              <div className="flex justify-between items-center bg-foreground w-full h-[100%] pl-16">
+                <div className="space-y-2">
+                  <div className="flex justify-start items-center gap-3">
+                    <FaApple className="text-background w-12 h-12" />
+                    <p className="text-background text-xs">{e.title}</p>
+                  </div>
+                  <div>
+                    <h1 className="text-background text-4xl leading-tight">
+                      {e.heading}
+                    </h1>
+                    <h1 className="text-background text-4xl leading-tight">
+                      off Voucher
+                    </h1>
+                  </div>
+                  <div className="flex justify-start items-center gap-2">
+                    <Link
+                      href=""
+                      className="text-background text-xs font-light border-b-[1px]"
+                    >
+                      Shop Now
+                    </Link>
+                    <FaArrowRight className="text-background text-xs" />
+                  </div>
+                </div>
+                <Image
+                  src={e.src}
+                  alt={"Hero Image"}
+                  className="h-full w-1/2"
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="absolute left-3 top-1/2" />
+        <CarouselNext className="absolute right-3 top-1/2" />
+        <div className="absolute bottom-2 left-1/2 -translate-x-1/2 flex justify-center items-center gap-2">
+          {heroIMages.map((e, i) => (
+            <span
+              className={`inline-block rounded-full ${
+                i === current
+                  ? "w-3 h-3 bg-secondary border-[1px] border-background"
+                  : "w-2 h-2 bg-secondary-foreground"
+              }`}
+              onClick={() => api && api.scrollTo(i)}
+            ></span>
+          ))}
+        </div>
+      </Carousel>
     </div>
   );
 }
