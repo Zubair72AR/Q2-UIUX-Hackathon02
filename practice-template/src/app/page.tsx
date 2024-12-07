f16ca9039eff2932bda6c0d13a29b7568d0f05de
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-12-08 01:54:42 +0500

    updated

--- practice-template/src/app/page.tsx
+++ practice-template/src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection";
+import ReverseTimer from "@/components/ReverseTimer";
 import TestButton from "@/components/TestButton";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <ReverseTimer seconds={172800} />
       <TestButton />
     </div>
   );
