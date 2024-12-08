90e6c44fa6f6ed780e3aacefc3a462a9a98fbaec
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-12-08 08:26:16 +0500

    updated

---
+++ final-hackathon2024/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
+        debitis tempora eaque maxime laborum repudiandae aut tempore fuga minus
+        id aliquid non aperiam, asperiores facilis nulla, natus accusantium
+        rerum nisi?
+      </h1>
+    </div>
+  );
+}
