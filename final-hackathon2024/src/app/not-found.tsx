d4b89545130b48712daeb7bd42d24258dd70ada0
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-12-21 17:30:35 +0500

    updated

--- final-hackathon2024/src/app/not-found.tsx
+++ final-hackathon2024/src/app/not-found.tsx
@@ -1,15 +1,23 @@
 "use client";
 import AllProductsLink from "@/components/AllProductsLink";
 import { OfferContext } from "@/components/Context";
+import { Button } from "@/components/ui/button";
 import React, { useContext } from "react";
 
 export default function NotFound() {
+  // Offer Strip Setup for Navbar Scrolling
   const { isOfferVisible, setIsOfferVisible } = useContext(OfferContext);
 
   return (
     <div className={`${isOfferVisible ? "mt-[110px]" : "mt-[74px]"} `}>
       <AllProductsLink bgColor="bg-white" lineColor="" />
-      <div className="h-[600px] grid place-items-center">Not Found</div>
+      <div className="flex justify-center items-center flex-col gap-3 my-48">
+        <h1 className="font-bold text-2xl text-red-500 leading-none">
+          Error 404
+        </h1>
+        <p>Oops! This page was not found</p>
+        <Button variant={"destructive"}>Back to Home</Button>
+      </div>
     </div>
   );
 }
