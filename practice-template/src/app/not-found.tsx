1b4f7f5ee115d4a94270766ce8d26fbea74d8a5e
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2025-01-09 19:37:59 +0500

    breadcrum setup

--- practice-template/src/app/not-found.tsx
+++ practice-template/src/app/not-found.tsx
@@ -1,22 +1,54 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { RxSlash } from "react-icons/rx";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 export default function notFound() {
-  const pathName = usePathname();
   return (
     <div className="py-8 md:py-12 lg:py-16 px-7 sm:px-8 lg:px-12 2xl:px-24">
-      <p className="text-sm font-medium text-zinc-500 capitalize">
-        {pathName.slice(1)} / <span className="text-foreground">404 Error</span>
-      </p>
+      {/* Bread Crum */}
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink
+              href="/"
+              className="text-sm font-medium text-zinc-500 capitalize"
+            >
+              Home
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <RxSlash />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbPage className="text-foreground font-medium">
+              404 Error
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      {/* Page Not Found */}
       <div className="grid place-items-center space-y-7 py-20">
+        {/* Main Heading */}
         <h2 className="text-center text-4xl sm:text-6xl font-medium">
           404 Not Found
         </h2>
+
+        {/* Description */}
         <p className="text-center text-sm">
           Your visited page not found. You may go home page.
         </p>
+
+        {/* Back to Home Button */}
         <Button>
           <Link href="/">Back to home page</Link>
         </Button>
--- practice-template/src/components/ToggleButton.tsx
+++ practice-template/src/components/ToggleButton.tsx
@@ -1,9 +1,6 @@
 "use client";
-
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
 
 export function ToggleButton() {
   const { theme, setTheme } = useTheme();
@@ -13,13 +10,13 @@ export function ToggleButton() {
   };
 
   return (
-    <Button variant="outline" size="icon" onClick={toggleTheme}>
+    <button onClick={toggleTheme}>
       {theme === "dark" ? (
-        <Sun className="h-[1.2rem] w-[1.2rem]" />
+        <Sun className="h-[1.25rem] w-[1.25rem]" />
       ) : (
-        <Moon className="h-[1.2rem] w-[1.2rem]" />
+        <Moon className="h-[1.25rem] w-[1.25rem]" />
       )}
       <span className="sr-only">Toggle theme</span>
-    </Button>
+    </button>
   );
 }
