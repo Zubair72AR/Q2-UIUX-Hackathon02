663a3d0069970f9c8ee5381012a97532512dc5df
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2025-01-11 00:30:48 +0500

    FlashSales

--- practice-template/src/components/FlashSales.tsx
+++ practice-template/src/components/FlashSales.tsx
@@ -5,9 +5,12 @@ import FlashSalesSlider from "./FlashSalesSlider";
 
 export default function FlashSales() {
   return (
-    <div className="px-7 sm:px-8 lg:px-12 2xl:px-24">
+    <div className="space-y-6 mt-24 px-7 sm:px-8 lg:px-12 2xl:px-24">
       <SubHeading text="Today’s" />
-      <CountdownTimer />
+      <div className="flex justify-start items-end gap-24">
+        <h2 className="text-4xl font-semibold">Flash Sales</h2>
+        <CountdownTimer />
+      </div>
       <FlashSalesSlider />
     </div>
   );
--- practice-template/src/components/FlashSalesSlider.tsx
+++ practice-template/src/components/FlashSalesSlider.tsx
@@ -5,6 +5,10 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import Image from "next/image";
+import { FaStar } from "react-icons/fa6";
+import { RiHeart3Line } from "react-icons/ri";
+import { PiEye } from "react-icons/pi";
 
 const FlashSalesItems = [
   {
@@ -15,6 +19,63 @@ const FlashSalesItems = [
     originalPrice: 160,
     stars: 5,
     reviews: 88,
+    category: "Gaming",
+    tag: "Flash Sales",
+  },
+  {
+    discount: 35,
+    image: "/flashSalesKeyboard.png",
+    name: "AK-900 Wired Keyboard",
+    price: 960,
+    originalPrice: 1160,
+    stars: 4,
+    reviews: 75,
+    category: "Computers",
+    tag: "Flash Sales",
+  },
+  {
+    discount: 40,
+    image: "/flashSalesGamepad.png",
+    name: "HAVIT HV-G92 Gamepad",
+    price: 120,
+    originalPrice: 160,
+    stars: 2,
+    reviews: 88,
+    category: "Gaming",
+    tag: "Flash Sales",
+  },
+  {
+    discount: 35,
+    image: "/flashSalesKeyboard.png",
+    name: "AK-900 Wired Keyboard",
+    price: 960,
+    originalPrice: 1160,
+    stars: 3,
+    reviews: 75,
+    category: "Computers",
+    tag: "Flash Sales",
+  },
+  {
+    discount: 40,
+    image: "/flashSalesGamepad.png",
+    name: "HAVIT HV-G92 Gamepad",
+    price: 120,
+    originalPrice: 160,
+    stars: 1,
+    reviews: 88,
+    category: "Gaming",
+    tag: "Flash Sales",
+  },
+  {
+    discount: 35,
+    image: "/flashSalesKeyboard.png",
+    name: "AK-900 Wired Keyboard",
+    price: 960,
+    originalPrice: 1160,
+    stars: 4,
+    reviews: 75,
+    category: "Computers",
+    tag: "Flash Sales",
   },
 ];
 
@@ -24,12 +85,66 @@ export default function FlashSalesSlider() {
       opts={{
         align: "start",
       }}
-      className="w-full max-w-sm"
+      className="w-full"
     >
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">aaaa</div>
+        {FlashSalesItems.map((item, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4">
+            <div className="space-y-1">
+              <div className="p-8 mb-3 h-[250px] bg-zinc-100 relative rounded-sm">
+                <p className="top-3 left-3 absolute px-3 py-1 font-medium text-primary-foreground text-[12px] bg-primary rounded-sm">
+                  -{item.discount}%
+                </p>
+                <div className="top-3 right-3 absolute text-xl rounded-full p-2 bg-background">
+                  <RiHeart3Line />
+                </div>
+                <div className="top-14 right-3 absolute text-xl rounded-full p-2 bg-background">
+                  <PiEye />
+                </div>
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={400}
+                  height={400}
+                  className="h-48 max-auto"
+                />
+              </div>
+              <p className="font-medium">{item.name}</p>
+              <div className="flex justify-start items-center gap-4">
+                <p className="text-primary font-medium">${item.price}</p>
+                <s className="text-zinc-500 font-medium">
+                  ${item.originalPrice}
+                </s>
+              </div>
+              <div className="flex justify-start items-center gap-[3px]">
+                <FaStar
+                  className={`${
+                    item.stars >= 1 ? "text-orange-400" : "text-zinc-300"
+                  }`}
+                />
+                <FaStar
+                  className={`${
+                    item.stars >= 2 ? "text-orange-400" : "text-zinc-300"
+                  }`}
+                />
+                <FaStar
+                  className={`${
+                    item.stars >= 3 ? "text-orange-400" : "text-zinc-300"
+                  }`}
+                />
+                <FaStar
+                  className={`${
+                    item.stars >= 4 ? "text-orange-400" : "text-zinc-300"
+                  }`}
+                />
+                <FaStar
+                  className={`${
+                    item.stars >= 5 ? "text-orange-400" : "text-zinc-300"
+                  }`}
+                />
+                <p className="font-medium text-xs text-zinc-500 pl-1">{`(${item.reviews})`}</p>
+              </div>
+            </div>
           </CarouselItem>
         ))}
       </CarouselContent>
