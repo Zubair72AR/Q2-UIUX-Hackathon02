f2d6290c045d8098ec7d8c9d0ca2ffb61e3266b2
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2025-01-16 12:41:02 +0500

    updated

--- practice-template/src/components/ExploreOurProducts.tsx
+++ practice-template/src/components/ExploreOurProducts.tsx
@@ -7,11 +7,8 @@ import { Button } from "./ui/button";
 export default function ExploreOurProducts() {
   return (
     <div className="space-y-6 mt-20 px-7 sm:px-8 lg:px-12 2xl:px-24">
-      <SubHeading text="Today’s" />
-      <div className="flex flex-col-reverse md:flex-row justify-start items-start md:items-end md:gap-20">
-        <h2 className="text-4xl font-semibold">Flash Sales</h2>
-        <CountdownTimer />
-      </div>
+      <SubHeading text="Our Products" />
+      <h2 className="text-4xl font-semibold">Flash Sales</h2>
       <FlashSalesSlider />
       <div className="grid place-items-center">
         <Button className="w-full sm:w-auto my-4">View All Products</Button>
