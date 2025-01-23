062b18704e8bb7bd5248b357505738385f9fa6d9
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2025-01-23 12:45:06 +0500

    updated

--- final-hackathon2024/src/sanity/env.ts
+++ final-hackathon2024/src/sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2021-08-31";
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-01-19";
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
@@ -15,11 +15,6 @@ function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage);
   }
+
   return v;
 }
-
-console.log({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-});
